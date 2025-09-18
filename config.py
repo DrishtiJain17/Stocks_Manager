852375e8e608445857cfd4c9f065fc312ddd035e
Drishti Jain <drishtijain1704@gmail.com>
2025-09-18 12:00:30 +0530

    config file + requirements.txt

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     #Replace "yourusername" with your PostgreSQL username and "yourpassword" with your PostgreSQL password
-    SQLALCHEMY_DATABASE_URI = 'postgresql://yourusername:yourpassword@localhost/stocks_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass1234@localhost/stocks_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'dh83ew9k'
